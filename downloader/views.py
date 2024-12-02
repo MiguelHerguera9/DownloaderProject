3f3948ca41dd5616112925990a52c0b36962e4e7
JABEHAU <jesusbe933@gmail.com>
2024-12-02 09:51:20 -0600

    feat: Music is downloaded in parallel.

--- downloader/views.py
+++ downloader/views.py
@@ -4,6 +4,7 @@ from django.views.decorators.csrf import csrf_exempt
 import json
 import os
 from yt_dlp import YoutubeDL
+import threading
 import time
 
 def index(request):
@@ -22,36 +23,51 @@ def process_urls(request):
             user_downloads_folder = os.path.join(os.environ.get('USERPROFILE', ''), 'Downloads')
             if not os.path.exists(user_downloads_folder):
                 return JsonResponse({'success': False, 'error': 'No se encontró la carpeta de descargas del usuario.'}, status=400)
-            
-            ydl_opts = {
-                'ffmpeg_location': 'C:\\ffmpeg\\bin',
-                'noplaylist': True,
-                'format': 'bestaudio/best',
-                'outtmpl': os.path.join(user_downloads_folder, '%(title)s.%(ext)s'),
-                'postprocessors': [
-                    {
-                        'key': 'FFmpegExtractAudio',
-                        'preferredcodec': 'mp3',
-                        'preferredquality': '192',
-                    }
-                ],
-            }
 
             download_files = []
+            errors = []
+            threads = []
 
-            with YoutubeDL(ydl_opts) as ydl:
-                for url in urls:
-                    info = ydl.extract_info(url, download=True)
-                    file_path = os.path.join(user_downloads_folder, f"{info['title']}.mp3")
+            def download_url(url):
+                """Descarga una URL específica utilizando YoutubeDL."""
+                try:
+                    # Opciones específicas por hilo
+                    ydl_opts = {
+                        'ffmpeg_location': 'C:\\ffmpeg\\bin',
+                        'noplaylist': True,
+                        'format': 'bestaudio/best',
+                        'outtmpl': os.path.join(user_downloads_folder, '%(id)s_%(title)s.%(ext)s'),
+                        'postprocessors': [
+                            {
+                                'key': 'FFmpegExtractAudio',
+                                'preferredcodec': 'mp3',
+                                'preferredquality': '192',
+                            }
+                        ],
+                    }
+                    with YoutubeDL(ydl_opts) as ydl:
+                        info = ydl.extract_info(url, download=True)
+                        file_path = os.path.join(user_downloads_folder, f"{info['id']}_{info['title']}.mp3")
 
-                    download_time = time.time()
-                    os.utime (file_path, (download_time, download_time))
+                        download_time = time.time()
+                        os.utime(file_path, (download_time, download_time))
 
-                    download_files.append(f"{info['title']}.mp3")
+                        download_files.append(file_path)
 
+                except Exception as e:
+                    errors.append({'url': url, 'error': str(e)})
 
-            return JsonResponse({'success': True, 'message': 'Descarga exitosa, los archivos se guardaron en la carpeta de descargas..', 'files': download_files})
+            for url in urls:
+                thread = threading.Thread(target=download_url, args=(url,))
+                threads.append(thread)
+                thread.start()
+
+            for thread in threads:
+                thread.join()
+
+            return JsonResponse({'success': True, 'message': 'Todas las descargas fueron exitosas.', 'files': download_files})
+        
         except json.JSONDecodeError:
-            return JsonResponse({'success': False, 'error': 'Ha ocurrido un error al descargar los archivos.'}, status=400)
+            return JsonResponse({'success': False, 'error': 'Error al procesar el cuerpo de la solicitud.'}, status=400)
 
     return JsonResponse({'success': False, 'error': 'Método no permitido.'}, status=405)
